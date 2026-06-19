7b1a5fb06548b67180bbe345edf1f8c81e1aba70
Brandon Nguyen <66812191+brandon-win@users.noreply.github.com>
2026-06-19 10:51:53 -0700

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const map = {}
+
+        for (let i = 0; i < nums.length; i++) {
+            if (map[nums[i]] === undefined) {
+                map[nums[i]] = 0
+            }
+
+            map[nums[i]]++
+        }
+
+        const topKEntries = Object.entries(map).sort((a, b) => a[1] - b[1]).slice(-k)
+        return topKEntries.map((e) => e[0])
+    }
+}
+
+// approach
+// create a map 
+// add numbers to the map as the key and increment frequency as the value
+// return entries and sort by frequency, slicing the top k entries
+// return res array of the keys of the top k 
+
+// approach 2 (max heap)
+// implement heap data structure (with array) 
+// create map
+// add all numbers(key) and freq(value)
+// list out entries and heapify 
+// return top k elements from heap (slice from first k elements)
